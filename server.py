d9728fb09c884bd59e9c54aac95ceb6815019059
Sanjeev Rijal <hopsanjeev@gmail.com>
2024-01-18 19:23:34 +1100

    final verson

--- server.py
+++ server.py
@@ -9,10 +9,11 @@ from wtforms import StringField, SubmitField, PasswordField, RadioField, SelectM
 from wtforms.validators import DataRequired, Email
 from datetime import date, timedelta
 from keys import flask_keys,sql_key
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = flask_keys
+app.config['SECRET_KEY'] = os.environ.get("flask_key")
 Bootstrap5(app)
 
 login_manager = LoginManager()
@@ -25,7 +26,7 @@ def load_user(user_id):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = sql_key
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI","sqlite:///Spending.db")
 db = SQLAlchemy()
 db.init_app(app)
 
